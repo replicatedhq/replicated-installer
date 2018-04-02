bb1ce5c4f521ced2ac98c15433ea741056af8015
SMITHA BALLAL <smithaballal@SMITHAs-MacBook-Pro.local>
2018-04-02 11:08:29 -0700

    update comments

--- install_scripts/templates/common/docker-version.sh
+++ install_scripts/templates/common/docker-version.sh
@@ -144,7 +144,7 @@ getMaxDockerVersion() {
             MAX_DOCKER_VERSION_RESULT="1.9.1"
         fi
     fi
-    # Amazon has their own repo and 1.12.6 and 17.06.2 are available there for now.
+    # Amazon Linux has Docker 17.06.2ce available to nearly all versions except 2017.03, which is at Docker 17.03.2ce
     if [ "$LSB_DIST" = "amzn" ]; then
         if [ "$_version" = "2017.03" ]; then 
            MAX_DOCKER_VERSION_RESULT="17.03.2"
