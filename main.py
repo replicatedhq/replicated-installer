fd5ba848950178f736b4557faaa2e2e85c0d2609
Kevin Herro <kevin@replicated.com>
2018-05-22 11:51:28 -0700

    init

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import boto3
 
 from install_scripts.app import app
 
+param.init(boto3.session.Session(region_name=os.getenv('AWS_REGION', 'us-east-1')))
+
 if __name__ == '__main__':
-    param.init(boto3.session.Session(region_name=os.getenv('AWS_REGION', 'us-east-1')))
     app.run(debug=(os.getenv('ENVIRONMENT') == 'dev'), host='0.0.0.0')
