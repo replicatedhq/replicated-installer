adc9877c7b533ff9e45aa2c12bf6bf42e6bec8f5
Kevin Herro <kevin@replicated.com>
2018-05-22 12:01:50 -0700

    import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 
+from install_scripts import param
 from install_scripts.app import app
 
 param.init(boto3.session.Session(region_name=os.getenv('AWS_REGION', 'us-east-1')))
