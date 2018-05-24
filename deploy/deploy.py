86d44b65d26179793f8f4c065ba6505e915f86fd
Andrew Reed <andrew@replicated.com>
2018-05-24 17:50:42 +0000

    addMessageToQueue is not defined

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -21,5 +21,5 @@ def add_message_to_queue(project, sha):
     q.write(m)
 
 
-addMessageToQueue("install-scripts",
+add_message_to_queue("install-scripts",
                   os.environ["CIRCLE_SHA1"][:7])
