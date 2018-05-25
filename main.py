89346ecfc53d4faee4bf89fb8f7becb9186e669c
Kevin Herro <kevin@replicated.com>
2018-05-25 10:32:03 -0700

    fix rebase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
+from install_scripts import param
 from install_scripts.app import app
+
+param.init()
 
 if __name__ == '__main__':
     app.run(debug=(os.getenv('ENVIRONMENT') == 'dev'), host='0.0.0.0')
