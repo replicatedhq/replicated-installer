f0851e25dcf40ab2cf747a50b669c742e9ffd2fe
Ethan Mosbaugh <ethan@replicated.com>
2018-06-26 12:11:42 -0700

    another oops

--- install_scripts/db.py
+++ install_scripts/db.py
@@ -16,10 +16,10 @@ def get():
 
     print("Connecting to db")
     database = param.lookup('MYSQL_DATABASE', '/mysql/database')
-    if database == '':
+    if not database:
         database = param.lookup('MYSQL_DB', '/mysql/database')
     password = param.lookup('MYSQL_PASSWORD', '/mysql/password', decrypt=True)
-    if password == '':
+    if not password:
         password = param.lookup('MYSQL_PASS', '/mysql/password', decrypt=True)
     db = g._database = mysql.connector.connect(
         host=param.lookup('MYSQL_HOST', '/mysql/host'),
