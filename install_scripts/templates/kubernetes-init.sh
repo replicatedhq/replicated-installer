d5122e7b28e3feaa6232ef42b250622eaf6e29e5
Andrew Reed <andrew@replicated.com>
2018-06-28 01:00:32 +0000

    configure weave encryption
    Encryption is enabled unless the network-encrypt=0 param is passed.
    On kernels below 4.2 print a warning that fast datapath will not be
    used when encryption is enabled.

--- install_scripts/templates/kubernetes-init.sh
+++ install_scripts/templates/kubernetes-init.sh
@@ -33,6 +33,7 @@ DEFAULT_SERVICE_CIDR="10.96.0.0/12"
 SERVICE_CIDR=$DEFAULT_SERVICE_CIDR
 DEFAULT_CLUSTER_DNS="10.96.0.10"
 CLUSTER_DNS=$DEFAULT_CLUSTER_DNS
+ENCRYPT_NETWORK=
 
 {% include 'common/common.sh' %}
 {% include 'common/prompt.sh' %}
@@ -189,6 +190,9 @@ getYAMLOpts() {
     if [ "$SERVICE_CIDR" != "$DEFAULT_SERVICE_CIDR" ]; then
         opts=$opts" no-proxy-address=$SERVICE_CIDR"
     fi
+    if [ "$ENCRYPT_NETWORK" = "0" ]; then
+        opts=$opts" encrypt-network=0"
+    fi
     YAML_GENERATE_OPTS="$opts"
 }
 
@@ -206,6 +210,22 @@ getK8sYmlGenerator() {
 
 weavenetDeploy() {
     logStep "deploy weave network"
+
+    sleeve=0
+    if [ "$ENCRYPT_NETWORK" != "0" ]; then
+        # Encrypted traffic cannot use the fast database on kernels below 4.2
+        kernel_major=$(uname -r | cut -d'.' -f1)
+        kernel_minor=$(uname -r | cut -d'.' -f2)
+        if [ "$kernel_major" -lt "4" ]; then
+            sleeve=1
+        elif [ "$kernel_major" -lt "5" ] && [ "$kernel_minor" -lt "3" ]; then
+            sleeve=1
+        fi
+
+        if [ "$sleeve" = "1" ]; then
+            printf "${YELLOW}This host will not be able to establish optimized network connections with other peers in the Kubernetes cluster.\nRefer to the Replicated networking guide for help.\n\nhttp://help.replicated.com/docs/kubernetes/customer-installations/networking/${NC}\n"
+        fi
+    fi
 
     sh /tmp/kubernetes-yml-generate.sh $YAML_GENERATE_OPTS weave_yaml=1 > /tmp/weave.yml
 
@@ -399,6 +419,9 @@ while [ "$1" != "" ]; do
             ;;
         cluster-dns|cluster_dns)
             CLUSTER_DNS="$_value"
+            ;;
+        encrypt-network|encrypt_network)
+            ENCRYPT_NETWORK="$_value"
             ;;
         *)
             echo >&2 "Error: unknown parameter \"$_param\""
