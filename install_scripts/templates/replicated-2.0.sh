7c20f64fbaff075d0cae9a97c56ad67cc81347d3
emosbaugh <emosbaugh@gmail.com>
2018-09-20 11:54:44 -0700

    Revert Docker restart policy on sysvinit docker service (#210)
    * Revert "check for no upstart rather than sysvinit, restart on failure [ch8216] (#207)"
    
    This reverts commit cbee37b8f608b1bb2ee34525b78c0bf88c2657a1.
    
    * Revert "restart policy on sysvinit docker service [ch8216] (#205)"
    
    This reverts commit c0fa7226ef9355b4cc64385b0bfca1d7794c024e.

--- install_scripts/templates/replicated-2.0.sh
+++ install_scripts/templates/replicated-2.0.sh
@@ -333,27 +333,12 @@ EOF
 }
 
 write_upstart_services() {
-    REPLICATED_RESTART_POLICY=
-    # NOTE: SysVinit does not support dependencies therefore we must add a
-    # restart policy to the replicated service. The tradeoff here is that
-    # SysVinit will lose track of the replicated process when docker restarts
-    # the replicated service.
-    if ! ls /etc/init/docker* 1> /dev/null 2>&1; then
-        REPLICATED_RESTART_POLICY="--restart on-failure:10"
-    fi
-
     cat > /etc/init/replicated.conf <<-EOF
 {% include 'upstart/replicated.conf' %}
-EOF
-    cat > /etc/init/replicated-stop.conf <<-EOF
-{% include 'upstart/replicated-stop.conf' %}
 EOF
 
     cat > /etc/init/replicated-ui.conf <<-EOF
 {% include 'upstart/replicated-ui.conf' %}
-EOF
-    cat > /etc/init/replicated-ui-stop.conf <<-EOF
-{% include 'upstart/replicated-ui-stop.conf' %}
 EOF
 }
 
