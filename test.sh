0ecb2910d26afa5b02525b284d821593935d4fb1
emosbaugh <emosbaugh@gmail.com>
2018-11-08 12:20:57 -0800

    Replicated should re-configure docker proxy address [ch15398] (#235)
    * Replicated should re-configure docker proxy address [ch15398]
    
    * rm -f
    
    * missing tests

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-./tests_shell/docker-version-test.sh
-
-./tests_shell/common-test.sh
-
 ./tests_shell/cli-script-test.sh
+./tests_shell/common-test.sh
+./tests_shell/docker-version-test.sh
+./tests_shell/ip-address-test.sh
+./tests_shell/proxy-test.sh
+./tests_shell/semver-test.sh
