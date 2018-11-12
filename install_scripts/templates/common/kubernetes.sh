283694bc8d126d4d45b582dccc40ed2c76e9a515
Andrew Reed <andrew@replicated.com>
2018-11-12 07:44:50 -0800

    do not fail dpkg on version dependency errors

--- install_scripts/templates/common/kubernetes.sh
+++ install_scripts/templates/common/kubernetes.sh
@@ -142,7 +142,7 @@ installKubernetesComponents() {
     case "$LSB_DIST$DIST_VERSION" in
         ubuntu16.04)
             export DEBIAN_FRONTEND=noninteractive
-            dpkg -i archives/*.deb
+            dpkg -i --force-depends-version archives/*.deb
             ;;
 
         centos7.4|centos7.5|rhel7.4|rhel7.5)
