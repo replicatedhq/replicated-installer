6d8b4604a96753426d860f850adfee86794f7ee8
Ethan Mosbaugh <ethan@replicated.com>
2018-12-31 12:15:23 -0800

    fix test

--- tests_shell/proxy-test.sh
+++ tests_shell/proxy-test.sh
@@ -170,7 +170,7 @@ test_getNoProxyAddresses()
 {
     ADDITIONAL_NO_PROXY="146.148.47.17"
     getNoProxyAddresses "10.128.0.39" "10.128.0.39/32"
-    assertEquals "localhost,127.0.0.1,172.17.0.1,146.148.47.17,10.128.0.39,10.128.0.39/32" "$NO_PROXY_ADDRESSES"
+    assertEquals "10.128.0.39,10.128.0.39/32,127.0.0.1,146.148.47.17,172.17.0.1,localhost" "$NO_PROXY_ADDRESSES"
 }
 
 . shunit2
