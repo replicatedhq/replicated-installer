bcfc6478eaf01702b83c55b5880babc10040d0b3
emosbaugh <emosbaugh@gmail.com>
2019-02-11 13:34:09 -0800

    Patch 18.09.1 Docker CVE-2019-5736 (#295)
    * Patch 18.09.1 Docker CVE-2019-5736
    
    * pinned version patches
    
    * roll back suse

--- install_scripts/templates/common/docker-install.sh
+++ install_scripts/templates/common/docker-install.sh
@@ -124,6 +124,7 @@ _installDocker() {
         compareDockerVersions "18.0.0" "${1}"
         if [ "$COMPARE_DOCKER_VERSIONS_RESULT" -eq "-1" ]; then
             if commandExists "amazon-linux-extras"; then
+                # NOTE: need to patch here with 18.09.2 or 18.06.2 when available.
                 ( set -x; amazon-linux-extras install -y -q docker=18.06.1 || amazon-linux-extras install docker=18.06.1 || \
                     amazon-linux-extras install -y -q docker || amazon-linux-extras install docker )
             else
@@ -146,7 +147,8 @@ _installDocker() {
         printf "${GREEN}Installing Docker from Zypper repository${NC}\n"
 
         # 2019-01-07
-        # SUSE has Docker 17.09.1_ce and Docker 18.06.1_ce available.
+        # SUSE has Docker 17.09.1_ce and Docker 18.09.0_ce available.
+        # NOTE: need to patch here with 18.09.2 or 18.06.2 when available.
         compareDockerVersions "18.0.0" "${1}"
         if [ "$COMPARE_DOCKER_VERSIONS_RESULT" -eq "-1" ]; then
             ( set -x; zypper -n install "docker=18.06.1_ce" || zypper -n install docker )
