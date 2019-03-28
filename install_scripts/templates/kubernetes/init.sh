15d9dfdd5b9e7cbfb5ea0135849d94eb12f3c0f3
Andrew Reed <areed@users.noreply.github.com>
2019-03-28 10:36:08 -0700

    restart kube-proxy (#332)
    This code was removed when we switched back to ipvs, but it's also
    needed for load balancer address changes.

--- install_scripts/templates/kubernetes/init.sh
+++ install_scripts/templates/kubernetes/init.sh
@@ -302,6 +302,10 @@ initKube() {
     if [ "$LOAD_BALANCER_ADDRESS_CHANGED" = "1" ]; then
         runUpgradeScriptOnAllRemoteNodes
         export KUBECONFIG=/etc/kubernetes/admin.conf
+
+        logStep "Restarting kube-proxy"
+        kubectl -n kube-system get pods | grep kube-proxy | awk '{print $1}' | xargs kubectl -n kube-system delete pod
+        logSuccess "Kube-proxy restarted"
     fi
 }
 
