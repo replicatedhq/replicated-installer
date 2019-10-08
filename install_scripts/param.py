1e575f8f198c56c64cb6eef2e0938dd7cb7b703d
Ethan Mosbaugh <ethan@replicated.com>
2019-10-08 17:56:05 +0000

    SSM params bulk lookup

--- install_scripts/param.py
+++ install_scripts/param.py
@@ -4,29 +4,50 @@ import boto3
 import os
 import sys
 
-from botocore.exceptions import ClientError
+param_cache = None
 
-param_cache = {}
+param_lookup = {
+    'ENVIRONMENT': '/replicated/environment',
+    'GRAPHQL_PREM_ENDPOINT': '/graphql/prem_endpoint',
+    'MIN_DOCKER_VERSION': '/install_scripts/min_docker_version',
+    'MYSQL_DATABASE': '/mysql/database',
+    'MYSQL_DB': '/mysql/database',
+    'MYSQL_PASSWORD': '/mysql/password',
+    'MYSQL_PASS': '/mysql/password',
+    'MYSQL_HOST': '/mysql/host',
+    'MYSQL_PORT': '/mysql/port',
+    'MYSQL_USER': '/mysql/user',
+    'NOT_FOUND': '/blah/blah',  # why not just to make sure
+    'PINNED_DOCKER_VERSION': '/install_scripts/pinned_docker_version',
+    'PINNED_KUBERNETES_VERSION': '/install_scripts/pinned_kubernetes_version',
+    'REGISTRY_ENDPOINT': '/registry_v2/advertise_address',
+    'REPLICATED_INSTALL_URL': '/replicated/installer_url',
+}
+
+
+class ParamCache:
+    def __init__(self, ssm):
+        self.ssm = ssm
+        self.m = {}
 
 
 def init():
     use_ssm = os.getenv("USE_EC2_PARAMETERS", '') != ''
     new_param_cache(use_ssm)
+    if use_ssm:
+        for env_name, val in get_parameters_from_ssm().items():
+            dict_set(env_name, val)
 
 
-def lookup(env_name, ssm_name, **kwargs):
-    if not param_cache:
+def lookup(env_name, **kwargs):
+    if param_cache is None:
         raise Exception('must initialize param cache')
 
-    if not param_cache['ssm'] or not ssm_name:
-        return os.getenv(env_name, kwargs.get('default', ''))
-
-    cached_val = dict_get(ssm_name)
+    cached_val = dict_get(env_name)
     if cached_val:
         return cached_val
 
-    val, ok = ssm_get(ssm_name, kwargs.get('decrypt', False))
-    return val if ok else kwargs.get('default', '')
+    return os.getenv(env_name, kwargs.get('default', ''))
 
 
 def new_param_cache(use_ssm):
@@ -37,35 +58,41 @@ def new_param_cache(use_ssm):
         sess = boto3.session.Session(
             region_name=os.getenv('AWS_REGION', 'us-east-1'))
         svc = sess.client('ssm')
-    param_cache = {'ssm': svc, 'm': {}}
+    param_cache = ParamCache(svc)
 
 
-def ssm_get(ssm_name, decrypt):
-    try:
-        resp = ssm_get_parameter(ssm_name, decrypt)
-    except ClientError as c:
-        if c.response['Error']['Code'] == 'ParameterNotFound':
-            return '', False
-        raise c
+def get_parameters_from_ssm():
+    ssm_names = []
+    params = {}
+    reverseLookup = {}
 
-    if not resp.get('Parameter'):
-        return '', False
+    for env_name, ssm_name in param_lookup.items():
+        if ssm_name != "":
+            ssm_names.append(ssm_name)
+            reverseLookup[ssm_name] = env_name
 
-    val = resp['Parameter']['Value']
-    dict_set(ssm_name, val)
-    return val, True
+    for names in list(chunk(ssm_names, 10)):
+        output = param_cache.ssm.get_parameters(
+            Names=names,
+            WithDecryption=True, )
 
+        for name in output['InvalidParameters']:
+            print("Ssm param {} invalid".format(name), file=sys.stderr)
 
-def ssm_get_parameter(ssm_name, decrypt):
-    return param_cache.get('ssm').get_parameter(
-        Name=ssm_name,
-        WithDecryption=decrypt
-    )
+        for param in output['Parameters']:
+            params[reverseLookup[param['Name']]] = param['Value']
 
-
-def dict_get(ssm_name):
-    return param_cache.get('m').get(ssm_name)
+    return params
 
 
-def dict_set(ssm_name, val):
-    param_cache.get('m').update({ssm_name: val})
+def dict_get(env_name):
+    return param_cache.m.get(env_name)
+
+
+def dict_set(env_name, val):
+    param_cache.m.update({env_name: val})
+
+
+def chunk(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
