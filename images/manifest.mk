6b9199547f347e31d99b7ca2bc98b910bc5ee792
Ethan Mosbaugh <ethan@replicated.com>
2020-05-12 20:11:52 +0000

    Scan envoy image

--- images/manifest.mk
+++ images/manifest.mk
@@ -1,5 +1,6 @@
 export CONTOUR_V0130 = gcr.io/heptio-images/contour:v0.13.0
 export COREDNS_131 = k8s.gcr.io/coredns:1.3.1
+export ENVOY_V1100 = replicated/envoy:v1.10.0-20200512
 export PAUSE_31 = k8s.gcr.io/pause:3.1
 export REGISTRY_262 = replicated/docker-registry:2.6.2-20200512
 export REPLICATED_HOSTPATH_PROVISIONER_CD1D272 = quay.io/replicated/replicated-hostpath-provisioner:cd1d272
