7e567366f927bda43919bff36a2c4ea8039aae26
Ethan Mosbaugh <ethan@replicated.com>
2020-05-12 16:48:20 +0000

    Build and scan docker/registry

--- images/manifest.mk
+++ images/manifest.mk
@@ -1,6 +1,7 @@
 export CONTOUR_V0130 = gcr.io/heptio-images/contour:v0.13.0
 export COREDNS_131 = k8s.gcr.io/coredns:1.3.1
 export PAUSE_31 = k8s.gcr.io/pause:3.1
+export REGISTRY_262 = replicated/docker-registry:2.6.2-20200512
 export REPLICATED_HOSTPATH_PROVISIONER_CD1D272 = quay.io/replicated/replicated-hostpath-provisioner:cd1d272
 export WEAVE_KUBE_252 = replicated/weave-kube:2.5.2-20200505
 export WEAVE_NPC_252 = replicated/weave-npc:2.5.2-20200507
