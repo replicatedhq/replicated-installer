15be111467c93e9b1eb901c54b882b1c2d2d5990
Ethan Mosbaugh <ethan@replicated.com>
2020-05-13 13:48:13 +0000

    Scan rook ceph

--- images/manifest.mk
+++ images/manifest.mk
@@ -1,3 +1,5 @@
+export CEPH_1420 = replicated/ceph:v14.2.0-20200512
+export CEPH_1422 = replicated/ceph:v14.2.2-20200512
 export CONTOUR_V0130 = gcr.io/heptio-images/contour:v0.13.0
 export COREDNS_131 = k8s.gcr.io/coredns:1.3.1
 export ENVOY_V1100 = replicated/envoy:v1.10.0-20200512
@@ -9,6 +11,8 @@ export KUBE_PROXY_V1153 = replicated/kube-proxy:v1.15.3
 export PAUSE_31 = replicated/pause:3.1
 export REGISTRY_262 = replicated/docker-registry:2.6.2-20200512
 export REPLICATED_HOSTPATH_PROVISIONER_CD1D272 = quay.io/replicated/replicated-hostpath-provisioner:cd1d272
+export ROOK_CEPH_103 = replicated/rook-ceph:v1.0.3-20200512
+export ROOK_CEPH_106 = replicated/rook-ceph:v1.0.6-20200512
 export WEAVEEXEC_252 = replicated/weaveexec:2.5.2-20200512
 export WEAVE_KUBE_252 = replicated/weave-kube:2.5.2-20200505
 export WEAVE_NPC_252 = replicated/weave-npc:2.5.2-20200507
