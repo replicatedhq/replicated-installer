f4ae1766f20a118c0ddad883cd1e75e1697165a2
Andrew Reed <andrew@replicated.com>
2020-06-04 16:54:40 +0000

    Update k8s common image

--- install_scripts/templates/common/kubernetes.sh
+++ install_scripts/templates/common/kubernetes.sh
@@ -530,7 +530,7 @@ airgapListKubernetesCommonImages1153() {
 airgapLoadKubernetesCommonImages1153() {
     docker run \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        "quay.io/replicated/k8s-images-common:v1.15.3-20200520"
+        "quay.io/replicated/k8s-images-common:v1.15.3-20200604"
 
     while read -r image; do
         (set -x; docker tag $image)
