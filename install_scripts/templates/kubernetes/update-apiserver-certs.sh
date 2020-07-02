474b3e3c0328f0b35d02a831cabbf968be48a8f0
Ethan Mosbaugh <ethan@replicated.com>
2020-07-02 21:53:19 +0000

    Fix update load balancer

--- install_scripts/templates/kubernetes/update-apiserver-certs.sh
+++ install_scripts/templates/kubernetes/update-apiserver-certs.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-PROXY_ADDRESS=
-PUBLIC_ADDRESS=
 PRIVATE_ADDRESS=
 LOAD_BALANCER_ADDRESS=
 LOAD_BALANCER_PORT=
-ADDITIONAL_SANS=
+KUBEADM_TOKEN="{{ kubeadm_token }}"
+KUBEADM_TOKEN_CA_HASH="{{ kubeadm_token_ca_hash }}"
+UNSAFE_SKIP_CA_VERIFICATION="{{ '1' if unsafe_skip_ca_verification else '0' }}"
+TAINT_CONTROL_PLANE="{{ '1' if taint_control_plane else '0' }}"
 
 {% include 'common/common.sh' %}
 {% include 'common/prompt.sh' %}
@@ -39,24 +40,23 @@ while [ "$1" != "" ]; do
     _param="$(echo "$1" | cut -d= -f1)"
     _value="$(echo "$1" | grep '=' | cut -d= -f2-)"
     case $_param in
-        http-proxy|http_proxy)
-            PROXY_ADDRESS="$_value"
-            ;;
         load-balancer-address|load_balancer_address)
             LOAD_BALANCER_ADDRESS="$_value"
             ;;
-        additional-sans|additional_sans)
-            if [ -z "$ADDITIONAL_SANS" ]; then
-                ADDITIONAL_SANS="$_value"
-            else
-                ADDITIONAL_SANS="$ADDITIONAL_SANS;$_value"
-            fi
-            ;;
-        public-address|public_address)
-            PUBLIC_ADDRESS="$_value"
-            ;;
         private-address|private_address)
             PRIVATE_ADDRESS="$_value"
+            ;;
+        kubeadm-token|kubeadm_token)
+            KUBEADM_TOKEN="$_value"
+            ;;
+        kubeadm-token-ca-hash|kubeadm_token_ca_hash)
+            KUBEADM_TOKEN_CA_HASH="$_value"
+            ;;
+        unsafe-skip-ca-verification|unsafe_skip_ca_verification)
+            UNSAFE_SKIP_CA_VERIFICATION=1
+            ;;
+        taint-control-plane|taint_control_plane)
+            TAINT_CONTROL_PLANE=1
             ;;
         *)
             echo >&2 "Error: unknown parameter \"$_param\""
@@ -66,19 +66,34 @@ while [ "$1" != "" ]; do
     shift
 done
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
+MASTER=1
 
+promptForToken
+promptForTokenCAHash
+
+kubernetesDiscoverPrivateIp
 if [ -z "$PRIVATE_ADDRESS" ]; then
     promptForPrivateIp
 fi
 
-# NOTE: there is no PUBLIC_ADDRESS or PROXY_ADDRESS prompts
-
 promptForLoadBalancerAddress
+API_SERVICE_ADDRESS="$LOAD_BALANCER_ADDRESS:$LOAD_BALANCER_PORT"
 
-# NOTE: this should use joinUpdateApiServerCerts for additional nodes
-# this will need a new bootstrap token
-updateApiServerCerts
+if [ "$TAINT_CONTROL_PLANE" != "1" ]; then
+    cp /etc/kubernetes/admin.conf /tmp/kube.conf
+    sed -i "s/server: https.*/server: https:\/\/$PRIVATE_ADDRESS:6443/" /tmp/kube.conf
+    export KUBECONFIG=/tmp/kube.conf
+
+    maybeSetTaintControlPlane
+fi
+
+rm -f /opt/replicated/kubeadm.conf
+makeKubeadmJoinConfigV1Beta2
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+joinUpdateKubeconfigs
+joinUpdateApiServerCerts
 
 outro
 
